6699b08c28c2ec329187bd3d0f43ae5dc64f3fed
Hakan GÜLEN <hgulen33@gmail.com>
2019-12-03 18:11:37 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ODM.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
